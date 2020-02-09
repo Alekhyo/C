df024b628f7306b58a743aefa1c01f141a06259a
Alekhyo Banerjee <59485605+Alekhyo@users.noreply.github.com>
2020-02-09 17:35:15 +0530

    Update binarySearch(Recursive).c

--- binarySearch(Recursive).c
+++ binarySearch(Recursive).c
@@ -3,20 +3,20 @@ int binarySearch(int A[],int start,int end,int num)
 {
     if(start<=end)
     {
-		int mid=(start+end)/2;                              /* Finding the middle element */
+		int mid=(start+end)/2;                              	/* Finding the middle element */
     	
 		if(A[mid]==num)
-        	return mid;										/* Checking the middle element */
+        	return mid;						/* Checking the middle element */
     	
 		else if(num>A[mid])
 			return binarySearch(A,mid+1,end,num);           /* Recursive Call to check the right half of the array if number greater than middle element*/
     		
 		else
-    		return binarySearch(A,start,mid-1,num);         /* Recursice Call to check the left half of the array if number is less than the middle element*/
+    		return binarySearch(A,start,mid-1,num);         	/* Recursice Call to check the left half of the array if number is less than the middle element*/
 	}
 	
 	else
-		return -1;                                         /* Returns -1 if element is not found */
+		return -1;                                         	/* Returns -1 if element is not found */
 }
 
 int main()
